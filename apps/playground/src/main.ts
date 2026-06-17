dba3f29b26b09d09e845de3f72c5a7aad0b7f114
tinas <ahmettinastepe0@gmail.com>
2026-06-17 18:54:29 +0300

    feat: add playground app

---
+++ apps/playground/src/main.ts
@@ -0,0 +1,14 @@
+import { createAioli } from '@aioli/vue'
+import { createPinia } from 'pinia'
+import { createApp } from 'vue'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(createAioli())
+
+app.mount('#app')
